30b6b816e192554152b494a90bc03680079d7ed4
DRs. <23101898@usc.edu.ph>
2025-09-04 14:46:22 +0800

    Create Array Stack Variation1.c

---
+++ Array Stack Variation1.c
@@ -0,0 +1,29 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX 10;
+
+typedef struct{
+    int items[MAX];
+    int top;
+}Stack;
+
+Stack* initialize();
+void push(Stack* s, int value);
+int pop(Stack* s);
+int peek(Stack* s);
+void display(Stack* s);
+
+int main() {
+    Stack* S = initialize();
+    return 0;
+}
+
+Stack* initialize(){
+    Stack *S = malloc 
+}
+int pop(Stack* s);
+int peek(Stack* s);
+void display(Stack* s);
+
