fd57cc063f682a8aea92e7ffd4ff2303324691d7
DRs. <23101898@usc.edu.ph>
2025-09-15 21:22:49 +0800

    Update Array Stack Variation1.c

--- Array Stack Variation1.c
+++ Array Stack Variation1.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#define MAX 10;
+#define MAX 10
 
 typedef struct{
     int items[MAX];
@@ -10,20 +10,71 @@ typedef struct{
 }Stack;
 
 Stack* initialize();
+bool isFull(Stack* s);
+bool isEmpty(Stack* s);
 void push(Stack* s, int value);
 int pop(Stack* s);
 int peek(Stack* s);
+int top(Stack *s);
 void display(Stack* s);
 
 int main() {
     Stack* S = initialize();
+    
+    push(S, 1);
+    push(S, 3);
+    push(S, 2);
+    push(S, 5);
+    push(S, 4);
+    int pops = pop(S);
+    printf ("%d\n", pops);
+    display(S);
+    int tops = top(S);
+    printf("\n%d", tops);
     return 0;
+}
+bool isFull(Stack* s){
+    if(s->top == MAX-1){
+        return true;
+    }
+    return false;
+}
+
+bool isEmpty(Stack* s){
+    if(s->top == -1){
+        return true;
+    }
+    return false;
+}
+
+void push(Stack* s, int value){
+    s->items[++(s->top)] = value;
+}
+
+int pop(Stack* s){
+    return s->items[(s->top)--];
+}
+
+int peek(Stack* s){
+    return s->items[s->top];
+}
+
+int top(Stack *s){
+    return s->top;
+}
+
+void display(Stack* s){
+    if(isEmpty(s)){
+        printf("Stack is Empty");
+    }else{
+        for(int i = 0; i <= s -> top; i++){
+            printf("%d ", s->items[i]);
+        }
+    }
 }
 
 Stack* initialize(){
-    Stack *S = malloc 
+    Stack *S = malloc(sizeof(Stack)); 
+    S->top = -1;
+    return S;
 }
-int pop(Stack* s);
-int peek(Stack* s);
-void display(Stack* s);
-
